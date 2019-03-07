45238fdfa7d9dc9cd3ac6b2bc7b15c146b96709b
daniel.michaelis <daniel.michaelis@iongroup.com>
2019-03-06 20:38:54 -0500

    project

---
+++ Layouts.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+console.ident = (v, l = "") => (console.log(l, v), v);
+
+export const Emphasis = ({ children }) => (
+  <span style={{ fontWeight: 900 }}>{children}</span>
+);
+
+// .columns > *:nth-child(n) ...
+
+export const Columns = count => ({ children }) => (
+  <div
+    className="columns"
+    style={{
+      maxHeight: "95vh",
+      maxWidth: "95vw",
+      // backgroundColor: 'tomato',
+      // padding: '5%',
+      // margin: "0 auto",
+      display: "grid",
+      gridTemplateColumns: `repeat(${count}, min-content)`, // What's the difference between min and max content?
+      gridAutoRows: "min-content",
+      gridColumnGap: "30px",
+      gridRowGap: "30px",
+      fontSize: "smaller"
+    }}
+  >
+    {React.Children.toArray(children)[0].props.children}
+  </div>
+);
+
+Columns.two = Columns(2);
+Columns.three = Columns(3);
+Columns.four = Columns(4);
--- theme.js
+++ theme.js
@@ -1,12 +1,9 @@
-import theme from 'mdx-deck/themes'
+import { future } from "mdx-deck/themes";
+import okaidia from "react-syntax-highlighter/styles/prism/okaidia";
 
 export default {
-  ...theme,
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
+  ...future,
+  prism: {
+    style: okaidia
+  }
+};
