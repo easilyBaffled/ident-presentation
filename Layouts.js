2779e63496881063990b95f93c8d316cea564c24
Danny Michaelis <dmichaelis0@gmail.com>
2019-05-07 20:15:15 -0400

    clean up Layout page

--- Layouts.js
+++ Layouts.js
@@ -1,26 +1,54 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import theme from './theme'
 
-export const Columns = count => ({ children }) => (
-    <div
-        className="columns"
-        style={{
-            maxHeight: '95vh',
-            maxWidth: '95vw',
-            // backgroundColor: 'tomato',
-            // padding: '5%',
-            // margin: "0 auto",
-            display: 'grid',
-            gridTemplateColumns: `repeat(${count}, max-content)`, // What's the difference between min and max content?
-            gridAutoRows: 'min-content',
-            gridColumnGap: '30px',
-            gridRowGap: '30px',
-            fontSize: 'smaller'
-        }}
-    >
-        {React.Children.toArray(children)[0].props.children}
-    </div>
+export const Grid = styled.div(( { count, area } ) => ( {
+    maxHeight: '95vh',
+    maxWidth: '95vw',
+    display: 'grid',
+    gridTemplateColumns: `repeat(${count}, max-content)`, // What's the difference between min and max content?
+    gridAutoRows: 'min-content',
+    gridColumnGap: 10,
+    gridRowGap: 20,
+    fontSize: 'smaller',
+    alignItems: 'center',
+    textAlign: 'left',
+    background: '#222',
+    margin: 'auto',
+    padding: 10,
+    "& pre > code span.token.console, & pre > code span.token.console+.token,  & pre > code span.token.console+.token+.property-access": {
+        color: '#0af',
+
+    },
+    ...(area ? { gridTemplateAreas: area } : {} ),
+    '& > *': {
+        height: '100%',
+        background: theme.colors.background,
+        boxSizing: 'border-box',
+        padding: '.8em 1em',
+        margin: '0',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    ...(area ? {
+    '& > *:nth-child(1n)': {
+        gridArea: 'a'
+    },
+    '& > *:nth-child(2n)': {
+        gridArea: 'b'
+    },
+    '& > *:nth-child(3n)': {
+        gridArea: 'c'
+    } } : {} ),
+} ) );
+
+export const Columns = count => ({ children, area }) => (
+    <Grid area={area} count={count} length={React.Children.toArray(children).length}>
+        {React.Children.toArray(children)}
+    </Grid>
 );
 
 Columns.two = Columns(2);
 Columns.three = Columns(3);
 Columns.four = Columns(4);
+
